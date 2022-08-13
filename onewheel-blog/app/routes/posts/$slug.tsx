42e67bd2836c047625728a77bc81b10b4d8c7c76
shitakemura <shitakemura@gmail.com>
2022-08-13 20:58:18 +0900

    show post.markdown

--- onewheel-blog/app/routes/posts/$slug.tsx
+++ onewheel-blog/app/routes/posts/$slug.tsx
@@ -1,3 +1,4 @@
+import { marked } from "marked";
 import { getPost } from "~/models/post.server";
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
@@ -6,15 +7,17 @@ import { useLoaderData } from "@remix-run/react";
 export const loader: LoaderFunction = async ({ params }) => {
   const { slug } = params;
   const post = await getPost(slug);
-  return json({ post });
+  const html = marked(post.markdown);
+  return json({ title: post.title, html });
 };
 
 export default function PostSlug() {
-  const { post } = useLoaderData();
+  const { title, html } = useLoaderData();
 
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+      <h1 className="my-6 border-b-2 text-center text-3xl">{title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
 }
