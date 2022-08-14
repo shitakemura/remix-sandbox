76355b4a49aa0034f08a78dcc50902fa657eb605
shitakemura <shitakemura@gmail.com>
2022-08-15 06:46:45 +0900

    fix ts error

--- onewheel-blog/app/routes/posts/admin/new.tsx
+++ onewheel-blog/app/routes/posts/admin/new.tsx
@@ -2,6 +2,13 @@ import { Form, useActionData } from "@remix-run/react";
 import type { ActionFunction } from "@remix-run/node";
 import { redirect, json } from "@remix-run/node";
 import { createPost } from "~/models/post.server";
+import invariant from "tiny-invariant";
+
+type ActionData = {
+  title: string | null;
+  slug: string | null;
+  markdown: string | null;
+};
 
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
 
@@ -12,22 +19,26 @@ export const action: ActionFunction = async ({ request }) => {
   const slug = formData.get("slug");
   const markdown = formData.get("markdown");
 
-  const errors = {
+  const errors: ActionData = {
     title: title ? null : "Title is required",
     slug: slug ? null : "Slug is required",
     markdown: markdown ? null : "Markdown is required",
   };
   const hasErrors = Object.values(errors).some((errorMessage) => errorMessage);
   if (hasErrors) {
-    return json(errors);
+    return json<ActionData>(errors);
   }
+
+  invariant(typeof title === "string", "title must be a string");
+  invariant(typeof slug === "string", "slug must be a string");
+  invariant(typeof markdown === "string", "markdown must be a string");
 
   await createPost({ title, slug, markdown });
   return redirect("/posts/admin");
 };
 
 export default function NewPost() {
-  const errors = useActionData();
+  const errors = useActionData() as ActionData;
 
   return (
     <Form method="post">
