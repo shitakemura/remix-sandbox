d95e24f9dff99decff56e3cdb87d554203f1d609
shitakemura <shitakemura@gmail.com>
2022-08-16 20:37:53 +0900

    hide admin routes fron non admin users

--- onewheel-blog/app/routes/posts/admin/index.tsx
+++ onewheel-blog/app/routes/posts/admin/index.tsx
@@ -1,4 +1,12 @@
 import { Link } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { requireAdminUser } from "~/session.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  await requireAdminUser(request);
+  return json({});
+};
 
 export default function AdminIndex() {
   return (
--- onewheel-blog/app/routes/posts/admin/new.tsx
+++ onewheel-blog/app/routes/posts/admin/new.tsx
@@ -1,8 +1,14 @@
 import { Form, useActionData, useTransition } from "@remix-run/react";
-import type { ActionFunction } from "@remix-run/node";
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { redirect, json } from "@remix-run/node";
 import { createPost } from "~/models/post.server";
 import invariant from "tiny-invariant";
+import { requireAdminUser } from "~/session.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  await requireAdminUser(request);
+  return json({});
+};
 
 type ActionData = {
   title: string | null;
@@ -13,6 +19,7 @@ type ActionData = {
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
 
 export const action: ActionFunction = async ({ request }) => {
+  await requireAdminUser(request);
   const formData = await request.formData();
 
   const title = formData.get("title");
