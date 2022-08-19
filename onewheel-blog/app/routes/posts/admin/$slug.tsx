c7616895418513d7f4cd2a450470efc23507eb03
shitakemura <shitakemura@gmail.com>
2022-08-19 22:17:47 +0900

    add edit form request

--- onewheel-blog/app/routes/posts/admin/$slug.tsx
+++ onewheel-blog/app/routes/posts/admin/$slug.tsx
@@ -6,7 +6,7 @@ import {
 } from "@remix-run/react";
 import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { redirect, json } from "@remix-run/node";
-import { createPost, getPost } from "~/models/post.server";
+import { createPost, getPost, updatePost } from "~/models/post.server";
 import invariant from "tiny-invariant";
 import { requireAdminUser } from "~/session.server";
 
@@ -53,7 +53,7 @@ export const action: ActionFunction = async ({ request, params }) => {
   if (params.slug === "new") {
     await createPost({ title, slug, markdown });
   } else {
-    // TODO: update post
+    await updatePost(params.slug, { title, slug, markdown });
   }
 
   return redirect("/posts/admin");
@@ -63,7 +63,9 @@ export default function NewPost() {
   const data = useLoaderData();
   const errors = useActionData() as ActionData;
   const transition = useTransition();
-  const isCreating = Boolean(transition.submission);
+  const isCreating = transition.submission?.formData.get("intent") === "create";
+  const isUpdating = transition.submission?.formData.get("intent") === "update";
+  const isNewPost = !data.post;
 
   return (
     <Form method="post" key={data.post?.slug ?? "new"}>
@@ -114,10 +116,13 @@ export default function NewPost() {
       <p className="text-right">
         <button
           type="submit"
+          name="intent"
+          value={isNewPost ? "create" : "update"}
           className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
-          disabled={isCreating}
+          disabled={isCreating || isUpdating}
         >
-          {isCreating ? "Creating..." : "Create Post"}
+          {isNewPost ? (isCreating ? "Creating..." : "Create Post") : null}
+          {isNewPost ? null : isUpdating ? "Updating..." : "Update Post"}
         </button>
       </p>
     </Form>
