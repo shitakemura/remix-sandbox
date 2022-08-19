448292cec504c9fb0009ba0b52ee8704af218f6b
shitakemura <shitakemura@gmail.com>
2022-08-20 07:27:08 +0900

    fix ts error

--- onewheel-blog/app/routes/posts/admin/$slug.tsx
+++ onewheel-blog/app/routes/posts/admin/$slug.tsx
@@ -12,17 +12,22 @@ import {
   getPost,
   updatePost,
 } from "~/models/post.server";
+import type { Post } from "~/models/post.server";
 import invariant from "tiny-invariant";
 import { requireAdminUser } from "~/session.server";
 
+type LoaderData = { post?: Post };
+
 export const loader: LoaderFunction = async ({ request, params }) => {
   await requireAdminUser(request);
+  invariant(params.slug, "slug is required");
+
   if (params.slug === "new") {
-    return json({});
+    return json<LoaderData>({});
   }
 
   const post = await getPost(params.slug);
-  return json({ post });
+  return json<LoaderData>({ post });
 };
 
 type ActionData = {
@@ -35,6 +40,8 @@ const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`
 
 export const action: ActionFunction = async ({ request, params }) => {
   await requireAdminUser(request);
+  invariant(params.slug, "slug is required");
+
   const formData = await request.formData();
   const intent = formData.get("intent");
 
@@ -71,7 +78,7 @@ export const action: ActionFunction = async ({ request, params }) => {
 };
 
 export default function NewPost() {
-  const data = useLoaderData();
+  const data = useLoaderData() as LoaderData;
   const errors = useActionData() as ActionData;
   const transition = useTransition();
   const isCreating = transition.submission?.formData.get("intent") === "create";
