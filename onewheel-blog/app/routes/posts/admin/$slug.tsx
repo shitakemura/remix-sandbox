66e5976c2a0b92d9760a45e6f17a3ab791b534d1
shitakemura <shitakemura@gmail.com>
2022-08-19 22:24:37 +0900

    add delete post request

--- onewheel-blog/app/routes/posts/admin/$slug.tsx
+++ onewheel-blog/app/routes/posts/admin/$slug.tsx
@@ -6,7 +6,12 @@ import {
 } from "@remix-run/react";
 import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { redirect, json } from "@remix-run/node";
-import { createPost, getPost, updatePost } from "~/models/post.server";
+import {
+  createPost,
+  deletePost,
+  getPost,
+  updatePost,
+} from "~/models/post.server";
 import invariant from "tiny-invariant";
 import { requireAdminUser } from "~/session.server";
 
@@ -31,6 +36,12 @@ const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`
 export const action: ActionFunction = async ({ request, params }) => {
   await requireAdminUser(request);
   const formData = await request.formData();
+  const intent = formData.get("intent");
+
+  if (intent === "delete") {
+    await deletePost(params.slug);
+    return redirect("/posts/admin");
+  }
 
   const title = formData.get("title");
   const slug = formData.get("slug");
@@ -65,6 +76,7 @@ export default function NewPost() {
   const transition = useTransition();
   const isCreating = transition.submission?.formData.get("intent") === "create";
   const isUpdating = transition.submission?.formData.get("intent") === "update";
+  const isDeleting = transition.submission?.formData.get("intent") === "delete";
   const isNewPost = !data.post;
 
   return (
@@ -113,7 +125,18 @@ export default function NewPost() {
           defaultValue={data.post?.markdown}
         />
       </p>
-      <p className="text-right">
+      <div className="flex justify-end gap-4">
+        {isNewPost ? null : (
+          <button
+            type="submit"
+            name="intent"
+            value="delete"
+            className="rounded bg-red-500 py-2 px-4 text-white hover:bg-red-600 focus:bg-red-400 disabled:bg-red-300"
+            disabled={isDeleting}
+          >
+            {isDeleting ? "Deleting..." : "Delete"}
+          </button>
+        )}
         <button
           type="submit"
           name="intent"
@@ -124,7 +147,7 @@ export default function NewPost() {
           {isNewPost ? (isCreating ? "Creating..." : "Create Post") : null}
           {isNewPost ? null : isUpdating ? "Updating..." : "Update Post"}
         </button>
-      </p>
+      </div>
     </Form>
   );
 }
